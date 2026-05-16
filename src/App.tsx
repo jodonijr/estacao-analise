3f85d9d3a4cc9bb92f803ce49d881ef645a6f1b3
jodonijr <jodonijr@hotmail.com>
2026-05-16 16:42:51 -0300

    fix: dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
 
   return (
     <SidebarProvider>
-      <div className="flex h-screen w-screen overflow-hidden bg-slate-50 text-slate-900">
+      <div className="flex h-screen w-screen overflow-hidden bg-background text-foreground">
         
         <MenuLateral 
           analiseSelecionada={analiseAtiva} 
